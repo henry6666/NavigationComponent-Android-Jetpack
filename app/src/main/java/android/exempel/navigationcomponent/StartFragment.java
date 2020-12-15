62339b9951850306e571ae83a11d7c3f44235f17
henry6666 <henry22@>
2020-12-15 17:15:10 +0100

    First commit

---
+++ app/src/main/java/android/exempel/navigationcomponent/StartFragment.java
@@ -0,0 +1,52 @@
+package android.exempel.navigationcomponent;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class StartFragment extends Fragment {
+
+
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.startGameButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navController.navigate(R.id.action_startFragment_to_gameFragment);
+
+            }
+        });
+
+    }
+}
